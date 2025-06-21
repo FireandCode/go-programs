d9bb07173acebf34d60d431a1bf19159ed1ed55f
pradeep patidar <pradeeppatidar1999@gmail.com>
2025-06-22 00:43:45 +0530

    limit service-time

--- go-concurrency-exercises/3-limit-service-time/main.go
+++ go-concurrency-exercises/3-limit-service-time/main.go
@@ -10,19 +10,72 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mu        sync.Mutex // For protecting channel initialization
+	ch        chan struct{} // For serializing requests per user
+}
+
+func ProcessUsingChannel(process func(), ch chan struct{})  {
+	process()
+	close(ch)
+
+}
+
+// getOrCreateChannel ensures the user's channel is initialized
+func (u *User) getOrCreateChannel() chan struct{} {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	if u.ch == nil {
+		u.ch = make(chan struct{}, 1) // Buffered channel with size 1
+	}
+	return u.ch
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+
+	if u.IsPremium {
+		process()
+		return true
+	}
+	
+	// Serialize requests for this user using channel
+	userCh := u.getOrCreateChannel()
+	userCh <- struct{}{} // Acquire the channel
+	defer func() {
+		<-userCh // Release the channel
+	}()
+	
+	// Now only one request can run at a time for this user
+	remainingTime := 10 - u.TimeUsed
+	if remainingTime <= 0 {
+		return false
+	}
+
+	start := time.Now()
+	ch := make(chan struct{})
+	go ProcessUsingChannel(process, ch)
+	// go process()
+	select {
+	case <-time.After(time.Duration(remainingTime) * time.Second):
+		u.TimeUsed += remainingTime
+		return false
+	case <-ch:
+		elapsedTime := time.Since(start).Seconds()
+		u.TimeUsed += int64(elapsedTime)
+		return true
+	}
 }
 
 func main() {
